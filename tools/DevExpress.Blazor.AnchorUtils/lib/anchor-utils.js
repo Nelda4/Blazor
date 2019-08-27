f9605340f7c54f6c77e0bda6fc5998044985153e
Yunina.Elena <elena.solovyeva@devexpress.com>
2019-08-27 14:54:30 +0300

    one project for server-side and client-side version

--- tools/DevExpress.Blazor.AnchorUtils/lib/anchor-utils.js
+++ tools/DevExpress.Blazor.AnchorUtils/lib/anchor-utils.js
@@ -5,8 +5,7 @@
         if (element) {
             var y = element.getBoundingClientRect().top + window.pageYOffset;
             /*The following code line updates a vertical scroll bar's offset for a standard Blazor Visual Studio template. 
-			Update the code to get an offset that is suitable for your application. 
-			*/
+			Update the code to get an offset that is suitable for your application.*/
             y -= document.querySelector(".main .top-row").offsetHeight;
             window.scroll(0, y);
         }
